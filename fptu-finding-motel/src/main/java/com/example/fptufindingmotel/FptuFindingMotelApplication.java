c7ac2fe38ab7d51c799677128aa7cbfdf1b76ca8
truongnqse05461 <truong10011998@gmail.com>
2020-05-20 22:29:52 +0700

    Signed-off-by: truongnqse05461 <truong10011998@gmail.com>
    first commit

---
+++ fptu-finding-motel/src/main/java/com/example/fptufindingmotel/FptuFindingMotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.fptufindingmotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FptuFindingMotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FptuFindingMotelApplication.class, args);
+    }
+
+}
---
+++ fptu-finding-motel/src/test/java/com/example/fptufindingmotel/FptuFindingMotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fptufindingmotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FptuFindingMotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
